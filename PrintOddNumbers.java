827f087f09aaabe0f288d31f8bb69c882b2ba113
kalearati <aratikale9297@gmail.com>
2024-12-22 16:36:30 +0530

    first

---
+++ PrintOddNumbers.java
@@ -0,0 +1,12 @@
+public class PrintOddNumbers {
+    public static void main(String[] args) {
+
+        int number = 1;
+
+        while (number <= 100) {
+            System.out.println(number);
+            number += 2;
+
+        }
+    }
+}
